b07b74ef7b3c605c362135acd9907e71b7344b90
frankhart2018 <choudhury.si@northeastern.edu>
2023-05-28 12:12:58 -0500

    Restrict special type to a select box

--- frontend/src/components/TypedTextField/TypedTextField.js
+++ frontend/src/components/TypedTextField/TypedTextField.js
@@ -4,15 +4,26 @@ import React, { forwardRef, useImperativeHandle, useState } from "react";
 const TypedTextField = forwardRef((props, ref) => {
   let [fieldType, setFieldType] = useState("integer");
   let [fieldValue, setFieldValue] = useState("");
+  let [isSpecialType, setIsSpecialType] = useState(false);
+  let [specialType, setSpecialType] = useState("get_stack");
 
   useImperativeHandle(ref, () => ({
     childFunction() {
       return {
         type: fieldType,
-        value: fieldValue,
+        value: isSpecialType ? specialType : fieldValue,
       };
     },
   }));
+
+  const onChangeFieldType = (e) => {
+    setFieldType(e.target.value);
+    if (e.target.value === "special") {
+      setIsSpecialType(true);
+    } else {
+      setIsSpecialType(false);
+    }
+  };
 
   return (
     <div>
@@ -21,22 +32,37 @@ const TypedTextField = forwardRef((props, ref) => {
         labelId="field-type-label"
         id="field-type"
         value={fieldType}
-        onChange={(e) => setFieldType(e.target.value)}
+        onChange={onChangeFieldType}
       >
         <MenuItem value="integer">Integer</MenuItem>
         <MenuItem value="object">Object</MenuItem>
         <MenuItem value="string">String</MenuItem>
         <MenuItem value="special">Special</MenuItem>
       </Select>
-      <TextField
-        label={props.fieldLabel}
-        variant="outlined"
-        sx={{
-          marginLeft: "10px",
-        }}
-        value={fieldValue}
-        onChange={(e) => setFieldValue(e.target.value)}
-      />
+      {!isSpecialType && (
+        <TextField
+          label={props.fieldLabel}
+          variant="outlined"
+          sx={{
+            marginLeft: "10px",
+          }}
+          value={fieldValue}
+          onChange={(e) => setFieldValue(e.target.value)}
+        />
+      )}
+      {isSpecialType && (
+        <>
+          <InputLabel id="special-type-label">Special Type</InputLabel>
+          <Select
+            labelId="special-type-label"
+            id="special-type"
+            value={specialType}
+            onChange={setSpecialType}
+          >
+            <MenuItem value="get_stack">Get from stack</MenuItem>
+          </Select>
+        </>
+      )}
     </div>
   );
 });
